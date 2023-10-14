4d46c7e0aa306889f78042bdd3b0c9cd9ce475b1
Teresa Vidal <teresa.vidal2001@alumnos.ubiobio.cl>
2023-10-14 16:50:12 -0300

    fix

--- config/routes.rb
+++ config/routes.rb
@@ -12,8 +12,8 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-  root "home#index"
+  root "home#index", as: :home
   get "home" => "home#index"
-  root 'appointments#welcome'
+  root 'appointments#welcome', as: :welcome
 
 end
