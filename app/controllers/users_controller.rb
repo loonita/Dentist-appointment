2bd5008e136c7d1b6638415ef509e0c9864a47c2
Teresa Vidal <teresa.vidal2001@alumnos.ubiobio.cl>
2023-11-06 12:55:33 -0300

    fix user only view their own users account

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,17 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :only_see_own_page, only: [:show]
   def show
     @user = User.find(params[:id])
     @appointments = Appointment.all.filter { |a| a.user_id == @user.id }
     @user_from_appointments = Appointment.find(params[:id])
 
   end
+  def only_see_own_page
+    @user = User.find(params[:id])
 
+    if current_user != @user
+      redirect_to root_path, notice: "Lo sentimos, pero sólo puedes ver tu propia página de perfil."
+    end
+  end
 end
