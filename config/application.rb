1bcfc80f40c76ef7e181429d5735ba0771f41039
Teresa Vidal <teresa.vidal2001@alumnos.ubiobio.cl>
2024-01-02 22:57:01 -0300

    config

--- config/application.rb
+++ config/application.rb
@@ -16,7 +16,8 @@ module Dentist
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w(assets tasks))
-
+    config.hosts << "146.83.198.35"
+    config.hosts << "146.83.198.35:1155"
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
